af333126430d5cc7469ffb61bc503b31ae59ea1e
Kuznetsov Timofey <t.kuznetsov@sptlab.net>
2026-02-06 15:05:15 +0300

    решение проблемы 412

--- downloader.py
+++ downloader.py
@@ -70,11 +70,13 @@ def download_video(url, output_path='downloads', quality='720', progress_callbac
                 'skip': ['hls', 'dash']
             }
         },
+        'impersonate': 'chrome',
         'http_headers': {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.9',
             'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-User': '?1',
+            'Sec-Fetch-Dest': 'document',
         },
         'progress_hooks': [hook],
     }
